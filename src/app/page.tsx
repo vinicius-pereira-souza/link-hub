62d84140fea61de810f4f681baae0e6ef9c1daa3
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2026-06-22 13:31:06 -0300

    chore(config): LinkHub project initial configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white font-bold">
+      <h1>Inciando um novo projeto</h1>
+    </div>
+  );
+}
