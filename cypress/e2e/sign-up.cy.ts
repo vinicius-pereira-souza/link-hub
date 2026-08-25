4d56f50bed82b24a6fce22f6533923b96d2a289f
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2026-08-25 18:53:15 -0300

    test(e2e): add new end-to-end tests for the sign-up flow
    - Create success scenarios for new user registration
    - Add validations for required fields and already registered emails
    - Ensure correct navigation after registration completion

--- cypress/e2e/sign-up.cy.ts
+++ cypress/e2e/sign-up.cy.ts
@@ -19,4 +19,60 @@ describe("sign-up authentication", () => {
 
     cy.get("h1", { timeout: 15000 }).should("be.visible");
   });
+
+  it("should display form field error messages if incorrect data is submitted.", () => {
+    cy.intercept("POST", "/api/auth/**").as("authRequest");
+
+    cy.visit("http://localhost:3000/sign-up");
+
+    cy.get("input[name='username']").type(" ");
+    cy.get("input[name='email']").type(" ");
+    cy.get("input[name='password']").type("123");
+    cy.get("input[name='confirmPassword']").type("1234");
+    cy.get("button[type='submit']").click();
+
+    cy.contains("span", "O nome de usuário deve ter pelo menos 6 caracteres.", {
+      timeout: 15000,
+    }).should("be.visible");
+
+    cy.contains("span", "Por favor, insira um e-mail válido.", {
+      timeout: 15000,
+    }).should("be.visible");
+
+    cy.contains("span", "A senha deve ter pelo menos 6 caracteres.", {
+      timeout: 15000,
+    }).should("be.visible");
+
+    cy.contains("span", "As senhas precisam ser iguais", {
+      timeout: 15000,
+    }).should("be.visible");
+  });
+
+  it("should display a quick message indicating a form submission error.", () => {
+    cy.intercept("POST", "/sign-up", (req) => {
+      if (req.headers["next-action"]) {
+        req.alias = "authRequest";
+      }
+    });
+
+    cy.visit("http://localhost:3000/sign-up");
+
+    cy.get("input[name='username']").type("user1234");
+    cy.get("input[name='email']").type("usuario1234.teste@example.com");
+    cy.get("input[name='password']").type("Password123!");
+    cy.get("input[name='confirmPassword']").type("Password123!");
+
+    cy.get("button[type='submit']").click();
+
+    cy.wait("@authRequest");
+
+    cy.get('[data-testid="flash-message-card"]', {
+      timeout: 15000,
+    })
+      .should("be.visible")
+      .and(
+        "contain.text",
+        "Houve um problema na realização do cadastro. Por favor, tente novamente mais tarde.",
+      );
+  });
 });
