8bd15a246287f81f81e8c010bc832849b4fc402a
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2026-09-06 14:14:24 -0300

    feat(ui): add new screen component for add-links

---
+++ src/app/(dashboard)/links/add-links/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div className="flex items-center">
+      <div className="flex-1 min-h-screen p-12">
+        <header className="flex items-end justify-between mb-12">
+          <div>
+            <h1 className="text-[32px] leading-10 font-semibold text-indigo-900">
+              Adicione seus links
+            </h1>
+            <h2 className="text-base leading-6 text-zinc-600">
+              Gerencie e organize o perfil do seu link público.
+            </h2>
+          </div>
+          <button>adicionar link</button>
+        </header>
+      </div>
+      <div className="flex-1 min-h-screen">teste</div>
+    </div>
+  );
+}
